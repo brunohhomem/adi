9963dd50c4dce21d27db888c2a5e15dce024e2a3
Bruno H. Homem <brunohhomem@gmail.com>
2024-06-11 06:26:50 -0300

    first commit

---
+++ src/test/java/br/com/bh/adi/AdiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bh.adi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
