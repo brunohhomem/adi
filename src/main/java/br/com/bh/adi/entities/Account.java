0d23c7a01cacdc213bb2c51fd23024a7d6dee77d
Bruno H. Homem <brunohhomem@gmail.com>
2024-06-17 18:45:45 -0300

    feat: added account entity

---
+++ src/main/java/br/com/bh/adi/entities/Account.java
@@ -0,0 +1,24 @@
+package br.com.bh.adi.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "tb_accounts")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Account {
+
+    @Id
+    @Column(name = "account_id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID accountId;
+
+    @Column(name = "description")
+    private String description;
+}
